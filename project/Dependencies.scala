7994e567dfc2ac2039d0e8c9ae52fb79cbbb5ab8
Scala Steward <scala_steward@virtuslab.com>
2024-11-09 20:04:38 +0000

    Update logback-classic to 1.5.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   lazy val munit = "org.scalameta" %% "munit" % "1.0.0-M8" % "it,test"
   lazy val fs2Core = "co.fs2" %% "fs2-core" % "3.5.0"
   lazy val fs2IO = "co.fs2" %% "fs2-io" % "3.5.0"
-  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.4.14"
+  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.5.12"
   lazy val sqlite = "org.xerial" % "sqlite-jdbc" % "3.45.0.0"
   lazy val fastparse = "com.lihaoyi" %% "fastparse" % "3.0.1"
   lazy val munitCatsEffects =
