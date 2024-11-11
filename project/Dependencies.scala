7901a1998f734657f54d75a6daa00e200ef5da75
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-11-11 12:44:42 +0100

    Update munit-cats-effect to 2.0.0 (#31)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
   lazy val sqlite = "org.xerial" % "sqlite-jdbc" % "3.45.3.0"
   lazy val fastparse = "com.lihaoyi" %% "fastparse" % "3.0.1"
   lazy val munitCatsEffects =
-    "org.typelevel" %% "munit-cats-effect" % "2.0.0-M1" % "it,test"
+    "org.typelevel" %% "munit-cats-effect" % "2.0.0" % "it,test"
   lazy val circeYaml = "io.circe" %% "circe-yaml-v12" % "0.14.3-RC3"
   lazy val circeGenericJVM = "io.circe" %% "circe-generic" % circeVersion
   lazy val log4cats = "org.typelevel" %% "log4cats-slf4j" % "2.6.0"
