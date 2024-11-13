d72f8ccae12519b4907e3f47e91ff36bb0f355c2
Scala Steward <scala_steward@virtuslab.com>
2024-11-13 18:21:26 +0000

    Update plasma-sdk to 0.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   lazy val plasmaOrg = "org.plasmalabs"
-  lazy val sdkVersion = "0.2.0"
+  lazy val sdkVersion = "0.2.1"
   val plasmaSdk = plasmaOrg %% "plasma-sdk" % sdkVersion
   val circeVersion = "0.15.0-M1"
 
